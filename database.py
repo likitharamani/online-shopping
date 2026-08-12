4fc6201e8008c4ad3758cae06c900a69220353d5
likitharamani <likitharamani2k5@gmail.com>
2026-08-12 22:22:32 +0530

    Secure database credentials

--- database.py
+++ database.py
@@ -1,13 +1,17 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 def get_db_connection():
 
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="online_shopping"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
 
     return connection
